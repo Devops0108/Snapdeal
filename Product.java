4632543e4cb974bc0daf8d0b6b4a9c08d92bc2e2
Devops0108 <prashanth.chintapally@gmail.com>
2023-08-09 06:35:59 +0530

    Created a file for Multiplication

---
+++ Product.java
@@ -0,0 +1,14 @@
+class Product
+{
+	public static void main(sting args[])
+	{
+		int n1,n2,res;
+		Scanner s=new scanner(system.in);
+		system.out.prinrln("Enter n1 value");
+		n1=s.nextInt();
+		system.out.println("Enter n2 value");
+		n2=s.nextInt();
+		res=n1*n2;
+		system.out.println("The Product value is:" +res);
+	}
+}
